a628ec88155541df5fc4c1ea028f54f780a45f95
lkkkGitHub <963087276@qq.com>
2018-09-07 16:08:11 +0800

    跳转页面

--- src/main/java/cn/qst/controller/TbUserController.java
+++ src/main/java/cn/qst/controller/TbUserController.java
@@ -1,13 +1,16 @@
 package cn.qst.controller;
 
+import cn.qst.pojo.TbUser;
 import cn.qst.service.TbUserService;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * @author lk
- * 2018/9/7 11:23
+ * @author lk 2018/9/7 11:23
  * @description:
  */
 
@@ -15,13 +18,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/User")
 public class TbUserController {
 
-    @Autowired
-    private TbUserService tbUserService;
+	@Autowired
+	private TbUserService tbUserService;
 
-    @RequestMapping("/login")
-    public String selectByPrimaryKey() {
-        tbUserService.selectByPrimaryKey();
-        return "redirect:/index.jsp";
-    }
-
+	@RequestMapping(value = "/login", method = { RequestMethod.GET })
+	public String login() {
+		return "login";
+	}
+	
+	@RequestMapping(value="/login", method = {RequestMethod.POST})
+	public String login(TbUser tbUser) {
+		
+		return "redirect:index.jsp";
+	}
+	
+	@RequestMapping(value="/regist", method = {RequestMethod.GET})
+	public String regist() {
+		return "regist";
+	}
+	
+	@RequestMapping(value="/regist", method = {RequestMethod.POST})
+	public String regist(TbUser tbUser) {
+		
+		return "login";
+	}
 }
